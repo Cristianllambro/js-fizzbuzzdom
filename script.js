a73955dfc685a016355cce197adfa8c0bf72083a
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-23 17:42:47 +0100

    finish work

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ let boxNumber = document.querySelector('.item-number');
 
 for (let i = 1; i <= 100; i++) {
     let item = document.createElement('div');
-    item.classList.add('box');
-    item.classList.add('number')
+    item.classList.add('box', 'number');
     item.innerHTML = i;
 
-    if (i % 3 == 0) {
+    if (i % 15 == 0) {
+        item.classList.add('fizzbuzz')
+        item.innerHTML = 'FizzBuzz'
+    } else if (i % 3 == 0) {
         item.classList.add('fizz')
         item.innerHTML = 'Fizz'
         
     } else if (i % 5 == 0) {
         item.classList.add('buzz')
         item.innerHTML = 'Buzz'
-    
-    } else if(i % (3 * 5) == 0) {
-        item.classList.add('fizzbuzz')
-        item.innerHTML = 'FizzBuzz'
     }
-
     boxNumber.append(item)
 }
