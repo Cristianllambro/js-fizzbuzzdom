7615e49559dc9a247aa14ee0f3165fa7b719b0e2
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-23 15:50:51 +0100

    new else

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let boxNumber = document.querySelector('.item-number');
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     let item = document.createElement('div');
     item.classList.add('box');
     item.classList.add('number')
@@ -8,13 +8,15 @@ for (let i = 0; i <= 100; i++) {
 
     if (i % 3 == 0) {
         item.classList.add('fizz')
+        document.writeln('fizz')
         
     } else if (i % 5 == 0) {
         item.classList.add('buzz')
     }
 
-    // } else ()
-    //     item.classList.add('fizzbuzz')
+    } else (i % 15 == 15){
+        item.classList.add('fizzbuzz')
+    }
     
     boxNumber.append(item)
 }
