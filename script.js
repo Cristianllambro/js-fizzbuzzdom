76ccbdbc7ff928e4bd6c15956d63f41dde40fcfc
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-23 15:34:07 +0100

    vorking js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let boxNumber = document.querySelector('.item-number');
 for (let i = 0; i <= 100; i++) {
     let item = document.createElement('div');
     item.classList.add('box');
+    item.classList.add('number')
     item.innerHTML = i;
 
-    if(i){
-
+    if (i % 3 == 0) {
+        item.classList.add('fizz')
+        
     }
     
     boxNumber.append(item)
